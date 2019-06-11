b57f892e4f7f59d4bbe03df8c1f1241e41e4b7e1
cdluznie <cdluznie@etud.u-pem.fr>
2019-06-11 13:08:13 +0200

    refactoring : add flask wrapper

--- motion_detector_server.py
+++ motion_detector_server.py
@@ -1,17 +1,15 @@
-from flask import Flask, Response
+from server import Server
 from video_feed import VideoFeed
 import atexit
 import utils
 
-videoFeed = VideoFeed.create(1)
-atexit.register(videoFeed.close)
-videoFeed.start()
-
-app = Flask(__name__)
-
-@app.route('/')
-def videoFeedWeb():
-	return Response(videoFeed.webGenerator(), mimetype='multipart/x-mixed-replace; boundary=frame')
+def create_motion_detector_server(name, videoFeed):
+	server = Server(name)
+	server.add_endpoint(
+		'/',
+		'index',
+		Response(videoFeed.webGenerator(), mimetype='multipart/x-mixed-replace; boundary=frame'))
+	return server
 
 if __name__ == '__main__':
 
@@ -21,5 +19,14 @@ if __name__ == '__main__':
 	port = 80
 
 	print('PUBLIC SERVER ACCESSIBLE FROM :', utils.get_ip_address(device) + ':' + str(port))
+	
+	# launch the video feed
+	videoFeed = VideoFeed.create(1)
+	videoFeed.start()
 
-	app.run(host='0.0.0.0', port=80)
+	# create and run the server
+	server = create_motion_detector_server(__name__, videoFeed)
+	server.run()
+
+	# stop the video feed when the server is stopped
+	videoFeed.close()
---
+++ server.py
@@ -0,0 +1,22 @@
+from flask import Flask, Response
+
+class EndpointResponse:
+
+	def __init__(self, response):
+		self.response = response
+
+	def __call__(self, *args):
+		return self.response
+
+class Server:
+
+	def __init__(self, name):
+		self.app = Flask(name)
+
+	def run(self):
+		# todo add port constructor 
+		app.run(host='0.0.0.0', port=80)
+
+	def add_endpoint(self, endpoint, endpointName, response):
+		self.app.add_url_rule(endpoint, endpointName, EndpointResponse(response))
+
